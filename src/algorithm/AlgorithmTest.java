8fa03016b152bb9f73875d1753404e42027847c8
Ryan Davis <3.1415927ryan@gmail.com>
2016-03-02 20:32:14 -0500

    fixed some stuff @pacevgod

--- src/algorithm/AlgorithmTest.java
+++ src/algorithm/AlgorithmTest.java
@@ -10,15 +10,20 @@ public class AlgorithmTest {
 		Site site = new Site("docs" + fileSeparator + "facebook_combined.txt");
 		Node.setSite(site);
 		site.setStart(0);
-		site.setEnd(500);
-		
+		site.setEnd(4040);
+
 		long time1 = System.currentTimeMillis();
 		ArrayList<Node> connection = Algorithm.processConnection(site); 
 		long time2 = System.currentTimeMillis();
-		
-		for (int i = 0; i < connection.size() - 1; i++)
-			System.out.print(connection.get(i).toString() + " --> ");
-		System.out.println(connection.get(connection.size() - 1));
+
+		if (connection == null) {
+			System.out.println("no connection found");
+		}
+		else {
+			for (int i = 0; i < connection.size() - 1; i++)
+				System.out.print(connection.get(i).toString() + " --> ");
+			System.out.println(connection.get(connection.size() - 1));
+		}
 		System.out.printf("Process took : %.3f seconds\n", (double) (time2 - time1) / 1000d);
 		System.out.printf("Process had %d file accesses\n", site.fileAccesses);
 	}
