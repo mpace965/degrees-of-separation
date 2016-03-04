262bccb91a4351b8d8f8bcde795ffda7db27836b
Thomas Fanella <fanella@purdue.edu>
2016-03-03 23:16:02 -0500

    Edited Test

--- src/databaseInterfacing/DBInterfaceTest.java
+++ src/databaseInterfacing/DBInterfaceTest.java
@@ -10,20 +10,19 @@ public class DBInterfaceTest {
 		Object[] values1 = {"sam", "2016-03-03 08:50:31"};
 		Object[] values2 = {"Tom", "2016-03-03 08:50:32"};
 		
-//		Object node1 = interfacer.addVertex("Node", props, values1);
-//		Object node2 = interfacer.addVertex("Node", props, values2);
-//		Object node3 = interfacer.addVertex("Node", props, values1);
-//		
-//		interfacer.addNewConnection("Connection", node1, node2);
-//		interfacer.addNewConnection("Connection", node2, node3);
+		Object node1 = interfacer.addVertex("Node", props, values1);
+		Object node2 = interfacer.addVertex("Node", props, values2);
+		Object node3 = interfacer.addVertex("Node", props, values1);
+		
+		interfacer.addNewConnection("Connection", node1, node2);
+		interfacer.addNewConnection("Connection", node2, node3);
 		
 		//interfacer.removeAllConnections();
-		Object node = interfacer.getVerticesByFields("Node", new String[]{"name"}, new Object[] {"Tom"});
-		//System.out.println(node);
+		//Object node = interfacer.getVerticesByFields("Node", new String[]{"name"}, new Object[] {"Tom"});
 		
-		interfacer.getConnectedNeighbors(node);
+		//interfacer.getConnectedNeighbors(node);
 		
-		//System.out.println(interfacer);
+		System.out.println(interfacer);
 		
 		interfacer.close();
 	}
