4f1b7d027f49061f89a8f886f22a22190c2602a8
Matthew Pace <pace4@purdue.edu>
2016-04-06 19:15:53 -0400

    Removed <br> tags, tweaked font size, removed Paper spacing on landingPage

--- client/dev/views/landingPage.jsx
+++ client/dev/views/landingPage.jsx
@@ -13,12 +13,9 @@ var LandingPage = React.createClass({
     return (
       <div className="landingPage">
         <Paper style={style} zDepth={1}>
-          <center> 
-              <font size="5"> Welcome to the Degrees of Separation website <br> Click connect to see how two objects are related! </br></font>
-          </center>
-        </Paper>
-        <Paper style={style} zDepth={1}>
-          <center> 
+          <center>
+              <p>Welcome to the Degrees of Separation website</p>
+              <p>Click connect to see how two objects are related!</p>
               <img src="http://cdn.zmescience.com/wp-content/uploads/2016/02/Six_degrees_of_separation_01.png" height="75%" width="75%"></img>
           </center>
         </Paper>
