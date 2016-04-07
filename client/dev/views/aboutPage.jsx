e0e7849405fc5d9842acec8a3f28a659c4a1764c
Matthew Pace <pace4@purdue.edu>
2016-04-06 20:01:38 -0400

    Removed <br> tags, tweaked font size, removed Paper spacing on aboutPage

--- client/dev/views/aboutPage.jsx
+++ client/dev/views/aboutPage.jsx
@@ -1,42 +1,34 @@
 var React = require('react');
 import Paper from 'material-ui/lib/paper';
 
-
-
 var AboutPage = React.createClass({
   render: function() {
-  
-  
     const style = {
       height: '75%',
       width: '75%',
       padding: 10,
-      margin: 2
+      margin: 20
     }
-    
-	
 
     return (
-    
-    
       <div className="aboutPage">
         <Paper style={style} zDepth={1}>
-          <div className="Title">
-          		<center> 
-          			<font size ="6"> About Page </font> 
-          		</center>
+          <div>
+            <center><h2>About</h2></center>
           </div>
-        </Paper>
-        <Paper style={style} zDepth={1}>
-          <div className="Does">
-          	 	<font size = "5"> What our App Does: </font>
-          		<font size = "3"> <br> <br> Our application is built around the concept of the six degrees of separation. We are using that concept to show that any two items can be linked together. </br> </br> </font> 	
-          		<font size = "5"> What is the Six Degrees of Separation: </font>	
-          		<font size = "3"> <br><br> The six degrees of separation is a theory that everyone and everything is six or fewer steps away by way of association. That is to say that a chain of "friends of friends" can be formed to connect any two people or things. </br></br> </font>
-          		<font size = "5"> About Us: </font>
-          		<font size = "3"> <br> <br> We are a group of students at Purdue University working on getting a degree in Computer Science. This website is our group project for CS 307 and if you want to see the development process it went though look at out github repo here <a href="https://github.com/mpace965/degrees-of-separation">GITHUB</a> </br> </br> </font> 	
+          <div>
+          	 	<h3>What our App Does</h3>
+          		<p>Our application is built around the concept of the six degrees of separation.
+                 We are using this concept to show that any two items in a given data set can be linked together.</p>
 
-          		
+          		<h3>What is the Six Degrees of Separation?</h3>
+          		<p>The theory of six degrees of separation is that everyone and everything is six or fewer steps away by way of association.
+                 That is to say that a chain of "friends of friends" can be formed to connect any two people or things.</p>
+
+          		<h3>About the Developers</h3>
+          		<p>We team a group of students at Purdue University studying Computer Science.
+                 This website is a result of a team effort for our Software Engineering class, <a href="https://www.cs.purdue.edu/homes/bxd/307/">CS 307</a>.
+                 You can view the full source of this website, and view a full history of our development on our <a href="https://github.com/mpace965/degrees-of-separation">GitHub repo.</a></p>
           </div>
         </Paper>
       </div>
