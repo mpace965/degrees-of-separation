465fb64367bfbc90b3c9bea315ab0d05b37bb6c7
epagryzi@purdue.edu <epagryzi@purdue.edu>
2016-04-07 14:04:36 -0400

    made nicer looking socail buttons

--- client/dev/views/statsView.jsx
+++ client/dev/views/statsView.jsx
@@ -3,8 +3,34 @@ var $ = require('jquery');
 import Paper from 'material-ui/lib/paper';
 import Snackbar from 'material-ui/lib/snackbar';
 import { FacebookButton, FacebookCount } from 'react-social';
+import {
+  ShareButtons,
+  ShareCounts,
+  generateShareIcon
+} from 'react-share';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton
+} = ShareButtons;
+
+const {
+  FacebookShareCount,
+  GooglePlusShareCount,
+  LinkedinShareCount
+} = ShareCounts;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const LinkedinIcon = generateShareIcon('linkedin');
+
 
 var StatsView = React.createClass({
+
+
   getInitialState: function() {
     return {
       stats: [],
@@ -45,6 +71,9 @@ var StatsView = React.createClass({
       margin: 20
     }
 
+    const shareUrl = 'http://degreeofconnection.com';
+    const title = 'View Your Connections At';
+    
     return (
       <div className="statsView">
         <Paper style={style} zDepth={1}>
@@ -55,7 +84,37 @@ var StatsView = React.createClass({
             <p>{this.state.stats[0]}</p>
           </div>
         </Paper>
-        <FacebookButton url="https://degreeofconnection.com">Share us on Facebook</FacebookButton>
+   
+        <div className="socialButtons">
+        
+         <FacebookShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button">
+            <FacebookIcon
+              size={32}
+              round={true} />
+          </FacebookShareButton>
+     <sp>     
+        <TwitterShareButton
+            url={shareUrl}
+            title={title}
+            className="Demo__some-network__share-button">
+            <TwitterIcon
+              size={32}
+              round={true} />
+          </TwitterShareButton>
+          </sp>
+          
+          <GooglePlusShareButton
+            url={shareUrl}
+            className="Demo__some-network__share-button">
+            <GooglePlusIcon
+              size={32}
+              round={true} />
+          </GooglePlusShareButton>
+        </div>
+        
         <Snackbar
           open={this.state.snackbarOpen}
           message={this.state.snackbarMessage}
